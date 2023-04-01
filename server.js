0ec0ae4596d94674920ef6ae301c0ec9a645477d
Muhammad Arbi Minanda <arbiminanda19@gmail.com>
2023-04-01 14:13:55 +0700

    basic get request with express

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", function (request, response) {
+  response.send("<h1>Hello world!</h1>");
+});
+
+app.get("/contact", function (req, res) {
+  res.send("contact me at arbiminanda10@gmail.com");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
